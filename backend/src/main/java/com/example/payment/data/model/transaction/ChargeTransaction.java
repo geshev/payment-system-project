70138ca9e360aba02c1a43ec90cc670504576a86
geshev <geshev.st@gmail.com>
2024-12-11 12:09:43 +0200

    The amount column should be nullable in order to accommodate reversal transactions

--- backend/src/main/java/com/example/payment/data/model/transaction/ChargeTransaction.java
+++ backend/src/main/java/com/example/payment/data/model/transaction/ChargeTransaction.java
@@ -3,6 +3,7 @@ package com.example.payment.data.model.transaction;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +15,8 @@ import java.math.BigDecimal;
 @DiscriminatorValue("CHARGE")
 public class ChargeTransaction extends Transaction {
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private BigDecimal amount;
 
     @Override
