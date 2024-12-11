d510fa1dfc4e155820a49da3dab8167efdb7d3ab
geshev <geshev.st@gmail.com>
2024-12-11 18:28:25 +0200

    Docker image for the backend

--- backend/src/main/java/com/example/payment/util/CSVUtils.java
+++ backend/src/main/java/com/example/payment/util/CSVUtils.java
@@ -4,8 +4,8 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
@@ -13,7 +13,7 @@ public class CSVUtils {
 
     public static <T> List<T> loadCSV(final String file, final Class<T> type) throws IOException {
         ClassPathResource resource = new ClassPathResource(file);
-        try (Reader reader = new FileReader(resource.getFile())) {
+        try (Reader reader = new InputStreamReader(resource.getInputStream())) {
             CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader)
                     .withType(type)
                     .withIgnoreLeadingWhiteSpace(true)
